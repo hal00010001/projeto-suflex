3883ad8a7880aa9eacea077ec91910b2a28aeef0
Almir Loiola Bindo <hal0001@gmail.com>
2022-04-26 00:16:54 -0300

    Typescript and Eslint done

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # How to run the project
 
+Clone the repository from the address https://github.com/hal00010001/projeto-suflex
+Run the command: npm install or yarn
+
 
 # Used technologies in the project
 
 Express
 Typescript
+Eslint
+PostgreSQL 13.4-R1 on Amazon AWS RDS
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
+const app = express();
 
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello World" })
-})
+app.get('/', (request, response) => response.json(
+  { message: 'Hello World' },
+));
 
-app.listen(3740)
+app.listen(3740);
